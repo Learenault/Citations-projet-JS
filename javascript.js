babd52a03569cfd145c2ac75df298f65b5db8e52
Renault lea <lea.renault@outlook.fr>
2021-09-10 11:53:13 +0200

    finalisation du projet citation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+// liste des differentes citations 
 const data = [
     {
         citation:'"Dans la vie on ne fait pas ce que l\'on veut mais on est responsable de ce que l\'on est."',
@@ -26,18 +28,62 @@ const data = [
 
 ];
 
+// Premiere methode pour changer le background
+const color=["rgb(209, 248, 209)","red","blue","black"];
+
+const randomcolor = () =>{
+const colors_lettres = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+const ref1 = Math.floor(Math.random() * 16);
+const ref2 = Math.floor(Math.random() * 16);
+const ref3 = Math.floor(Math.random() * 16);
+const ref4 = Math.floor(Math.random() * 16);
+const ref5 = Math.floor(Math.random() * 16);
+const ref6 = Math.floor(Math.random() * 16);
+ let ref ='#'+colors_lettres[ref1]+colors_lettres[ref2]+colors_lettres[ref3]+colors_lettres[ref4]+colors_lettres[ref5]+colors_lettres[ref6];
+ return ref
+}
+
+
+// Deuxime methode pour changer la couleur de fond -> et celle retenue !
+const hslcolor = () =>{
+    const color1 = Math.floor(Math.random() * 360);
+    let color_ref = 'hsl('+color1+',70%,85%)';
+    cita.setAttribute("style","background-color:"+color_ref)
+    return cita;
+}
+
+
+// Troisieme methode pour changer la couleur de fond
+const update_color = () =>{
+    let change_color = color[couleur];
+    if(couleur == color.length){
+         couleur = 0;
+         change_color = color[couleur];   
+    }
+couleur++;
+cita.setAttribute("style","background-color:"+change_color)
+return  cita
+}
+
+
+
+// création des variables 
 const button = document.querySelector('.title');
 const text = document.querySelector('.text p');
 const annee = document.querySelector('.age p');
 const prof = document.querySelector('.profession p');
 const auth = document.querySelector('.author');
-
+const cita = document.querySelector('.citation');
 let index = 0;
+couleur = 0;
 
+
+
+
+// fonction pour générer les citations
 const generate = () =>{
     let citation = data[index];
-    let longueur = data.length;
-    if(index == longueur){
+    if(index == data.length){
         index = 0;
          citation = data[index];   
     }
@@ -45,6 +91,11 @@ index++;
 return citation;
 }
 
+
+
+
+
+// Les fonctions sont appelés lors du click sur le bouton
 button.addEventListener('click', ()=>{
 
 let {citation,author,age,profession} = generate();
@@ -54,6 +105,16 @@ auth.textContent = author;
 annee.textContent = age;
 prof.textContent = profession;
 
+//console.log(update_color());
+//console.log(randomcolor());
+//let txt_color = randomcolor()
+//cita.setAttribute("style","background-color:"+randomcolor())
+//text.setAttribute("style","color:"+txt_color);
+//prof.setAttribute("style","color:"+txt_color);
+//auth.setAttribute("style","color:"+txt_color);
+//annee.setAttribute("style","color:"+txt_color);
+
+hslcolor();
 });
 
 
